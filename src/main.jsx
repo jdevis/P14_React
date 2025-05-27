3ece7ea246f36ff8f5e8268e45fb66943b824cbb
jdevis <josephine.roubertie@gmail.com>
2025-05-27 11:46:43 +0200

    init project

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import EmployeesList from './pages/EmployeesList'
+import Error from './pages/Error'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/employeesList" element={<EmployeesList />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+  </StrictMode>,
+)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <header>
+        <h1>HRnet</h1>
+      </header>
+      <main>
+        <div className='container'>
+          <h2>Create Employee</h2>
+          <form>
+            <label htmlFor='first-name'>First Name</label>
+            <input id='first-name' />
+
+            <label htmlFor='last-name'>Last Name</label>
+            <input type='text' id='last-name' />
+
+            <label htmlFor='date-of-birth'>Date of Birth</label>
+            <input id='date-of-birth' type='text' />
+
+            <label htmlFor='start-date'>Start Date</label>
+            <input id='start-date' type='text' />
+
+            <fieldset className='address'>
+              <legend>Address</legend>
+
+              <label htmlFor='street'>Street</label>
+              <input id='street' type='text' />
+
+              <label htmlFor='city'>City</label>
+              <input id='city' type='text' />
+
+              <label htmlFor='state'>State</label>
+              <select name='state' id='state'></select>
+
+              <label htmlFor='zip-code'>Zip Code</label>
+              <input id='zip-code' type='number' />
+            </fieldset>
+
+            <label htmlFor='department'>Department</label>
+            <select name='department' id='department'>
+              <option>Sales</option>
+              <option>Marketing</option>
+              <option>Engineering</option>
+              <option>Human Resources</option>
+              <option>Legal</option>
+            </select>
+            <button>Save</button>
+          </form>
+
+        </div>
+        <div id='confirmation' className='modal'>Employee Created!</div>
+      </main>
+    </>
+  )
+}
+
+export default Home
